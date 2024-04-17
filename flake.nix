6421ea17a9f31ee08f8ef62e773fb23dc1c1533e
Adrian Gierakowski <agierakowski@gmail.com>
2024-04-17 12:56:11 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Basic usage of flakey-profile";
+
+  inputs = {
+    flakey-profile.url = "github:lf-/flakey-profile";
+    nixpkgs.url = "github:nixos/nixpkgs?rev=ff0dbd94265ac470dda06a657d5fe49de93b4599";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flakey-profile }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        nix = pkgs.nixVersions.nix_2_21;
+      in
+      {
+        # Any extra arguments to mkProfile are forwarded directly to pkgs.buildEnv.
+        #
+        # Usage:
+        # Switch to this flake:
+        #   nix run .#profile.switch
+        # Revert a profile change (note: does not revert pins):
+        #   nix run .#profile.rollback
+        # Build, without switching:
+        #   nix build .#profile
+        # Pin nixpkgs in the flake registry and in NIX_PATH, so that
+        # `nix run nixpkgs#hello` and `nix-shell -p hello --run hello` will
+        # resolve to the same hello as below [should probably be run as root, see README caveats]:
+        #   sudo nix run .#profile.pin
+        packages.profile = flakey-profile.lib.mkProfile {
+          inherit pkgs;
+          # Specifies things to pin in the flake registry and in NIX_PATH.
+          pinned = { nixpkgs = toString nixpkgs; };
+          paths = with pkgs; [
+            nix
+            direnv
+            (nix-direnv.override { inherit nix; })
+          ];
+        };
+      });
+}
+
