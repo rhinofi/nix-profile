006e4f18c503d2341a02e10f218c9e50794a8e0c
Adrian Gierakowski <agierakowski@gmail.com>
2024-12-11 11:34:05 +0000

    update nixpkgs and nix to 2.24

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flakey-profile.url = "github:lf-/flakey-profile";
-    nixpkgs.url = "github:nixos/nixpkgs?rev=b9014df496d5b68bf7c0145d0e9b0f529ce4f2a8";
+    nixpkgs.url = "github:nixos/nixpkgs?rev=566e53c2ad750c84f6d31f9ccb9d00f823165550";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -13,7 +13,7 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        nix = pkgs.nixVersions.nix_2_23;
+        nix = pkgs.nixVersions.nix_2_24;
       in
       {
         # Any extra arguments to mkProfile are forwarded directly to pkgs.buildEnv.
